26d1da817186e97768867b28fcdbefc78017c549
spreadzenox <hpesquetjulien@gmail.com>
2024-01-15 19:41:11 +0100

    jsp

--- dataset.py
+++ dataset.py
@@ -13,7 +13,7 @@ from torchvision.transforms import Compose, Resize, ToTensor, Normalize
 
 
 class GestRecog(torch.utils.data.Dataset):
-    def __init__(self, dataDir='./leapGestRecog/', transform=None, crossNum=None, crossIDs=None):
+    def __init__(self, dataDir='./Personnal_data/', transform=None, crossNum=None, crossIDs=None):
         # Initialize the data and label list
         self.labels = []
         self.data = []
@@ -26,55 +26,26 @@ class GestRecog(torch.utils.data.Dataset):
 
         # First load all images data
         import os
-        path_ciseau="./ciseau/"
-        path_poing="./poing/"
-        liste_poing=os.listdir(path_poing)
-        liste_ciseau=os.listdir(path_ciseau)
-        liste_sujet = os.listdir(dataDir)
-        liste_geste = [[dataDir + i + '/' + j for j in os.listdir(dataDir + i)] for i in liste_sujet]
-        liste_image = []
-        liste_label = []
-        for image in liste_ciseau:
-            image = Image.open(path_ciseau+image).convert('L')
-            if self.transform is not None:
-                image_tensor = self.transform(image)
-            self.data_perso.append(image_tensor)
-            label_tensor = torch.zeros(2)
-            label_tensor[0] = 1
-            self.labels_perso.append(label_tensor)
-        for image in liste_poing:
-            image = Image.open(path_poing+image).convert('L')
-            if self.transform is not None:
-                image_tensor = self.transform(image)
-            self.data_perso.append(image_tensor)
-            label_tensor = torch.zeros(2)
-            label_tensor[1] = 1
-            self.labels_perso.append(label_tensor)
-        for sujet in liste_geste:
-            for geste in sujet:
-                for image in os.listdir(geste):
-                    liste_image.append(geste + '/' + image)
-                    liste_label.append(geste)
-        for x in tqdm(liste_image):
-            # Read the data using PIL
-            image = Image.open(x).convert('L')
-            if self.transform is not None:
-                image_tensor = self.transform(image)
-            temp.append(image_tensor)
+        liste_categories = os.listdir(dataDir)
+        liste_images = [[dataDir +i +"/"+ j for j in os.listdir(dataDir + i)] for i in liste_categories]
+        for cate in tqdm(liste_images):
+            for image_path in cate:
+                image = Image.open(image_path).convert('L')
+                if self.transform is not None:
+                    image_tensor = self.transform(image)
+                self.data_perso.append(image_tensor)
+                
+                label = int(image_path.split("/")[2]) - 1
+                label_tensor = torch.zeros(10)
+                label_tensor[label] = 1
+                self.labels_perso.append(label_tensor)
 
             # Second filter according name for labelling : 1 = palm 02=l 03=fist 04=fist_moved 05=thumb  06=index 07=ok
             # 08=palm_moved 09=c 10=down
 
             # Get label in the path and convert it
-            label_path = liste_label[liste_image.index(x)]
-            label_path_splitted = label_path.split("/")
-            label = label_path_splitted[3][:2]
-            label = int(label) - 1
+
             # Create a one hot tensor with 1 in the corresponding index
-            label_tensor = torch.zeros(10)
-            label_tensor[label] = 1
-            # Append
-            tempL.append(label_tensor)
 
         if crossNum is not None:
             totalLength = len(temp)
@@ -116,7 +87,6 @@ if __name__ == '__main__':
     ])
     cd = GestRecog(transform=tr, crossNum=None, crossIDs=None)
     print(len(cd))
-    image, label = next(iter(cd))
     torch.save(cd.data, 'images.pth')
     torch.save(cd.data_perso,"images_perso.pth")
     torch.save(cd.labels, 'labels.pth')
