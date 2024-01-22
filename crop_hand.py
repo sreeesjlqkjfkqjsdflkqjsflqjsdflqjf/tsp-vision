f388fb330adc0b371095bcfc25e2b8a3eb559b81
Nawi <59661951+nnnawi@users.noreply.github.com>
2024-01-23 00:40:29 +0100

    Add files via upload

--- crop_hand.py
+++ crop_hand.py
@@ -1,5 +1,7 @@
 import cv2
 import mediapipe as mp
+import os
+from tqdm import tqdm
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands()
@@ -30,6 +32,7 @@ def crop_hand_mp(input_img): #Input : path vers photo (string)
     frame = cv2.imread(input_img)
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = hands.process(rgb_frame)
+
     if results.multi_hand_landmarks:
         hand_landmarks = results.multi_hand_landmarks[0].landmark
 
@@ -42,9 +45,43 @@ def crop_hand_mp(input_img): #Input : path vers photo (string)
             y_min = min(y_min, y) 
             x_max = max(x_max, x) 
             y_max = max(y_max, y)
+        
+        img_width = abs(x_max - x_min)
+        img_height = abs(y_max - y_min)
 
-        if x_max > x_min and y_max > y_min:
-            hand_cropped = frame[y_min-pad:y_max+pad, x_min-pad:x_max+pad]
+        print(img_width)
+        
+        hand_cropped = frame[y_min-pad:y_max+pad, x_min-pad:x_max+pad]  
+
+        if img_width < 70 or img_height < 70:
+            return frame
+        else : 
             return hand_cropped
-        else:
-            return frame #Output : image np-array
+             
+
+dataset_path = "augmented-rps-dataset/"
+output_folder = "test/"
+
+def create_cropped_dataset(file_path):
+    dirs = [i[0] for i in os.walk(file_path)][1:]
+
+    sub_dirs = [[i[2] for i in os.walk(j)][-1] for j in dirs]
+
+    for index, i in enumerate(sub_dirs):
+        print('Traitement du dossier {} \n'.format(index))
+        
+        os.makedirs('{}{}'.format(output_folder,index))
+        fail_count = 0
+
+        for j in tqdm(i[:4]):
+            current_img_path = dirs[index] + "/" + j
+            try:
+                cropped_img = crop_hand_mp(current_img_path)
+                cv2.imwrite('{}{}/{}'.format(output_folder,index,j), cropped_img)
+                #print('{}{}/{}'.format(output_folder,index,j))
+            except:
+                fail_count += 1
+        
+        print('{} erreurs pour {} images'.format(fail_count,len(i)))
+
+create_cropped_dataset(dataset_path)
